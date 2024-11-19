2f18922b310679efeea0903eb0444aef36de0094
Nikhiloooo <nikhilmohan710@gmail.com>
2024-11-19 13:17:43 +0530

    bank

---
+++ src/com/quest/oops/bank/Transaction.java
@@ -0,0 +1,12 @@
+package com.quest.oops.bank;
+
+public abstract class Transaction {
+
+    protected BankAccount account;
+
+    public Transaction(BankAccount account) {
+        this.account = account;
+    }
+
+    public abstract void processTransaction(double amount);
+}
